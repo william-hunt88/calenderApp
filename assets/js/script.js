3c675be63381068a3fe02ce01060fe23fa2262d0
w.hunt <wfh2d88@gmail.com>
2021-01-21 13:41:24 -0600

    almost done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,32 @@
-var timeDisplay = document.querySelector(".timeDisplay").innerHTML;
 document.getElementById("currentDay").innerHTML = moment().format("MMMM Do YYYY");
- 
+
+// loads tasks from local storage
+for(var i = 9; i < 13; i++){
+  $(`#hour-${i} .description`).val(localStorage.getItem(i));
+
+};
 
 
-var auditTime = function(timeEl) {
-    var eventTime = moment(timeDisplay, "h:mm a")._i;
-    var currentTime = moment().format("h:mm a");
-
-    console.log(currentTime);
-    console.log(eventTime);
-
-    if (moment(currentTime).isAfter(eventTime)) {
-      console.log("its after 9 baby")
-
-  };
-
-}
-
-// audit task due dates every 30 minutes
+// audit task due dates every minute
 setInterval(function() {
-    $(".timeDiv .timeDisplay").each(function() {
-      auditTime($(this));
-    });
-  }, 10000);
+    for(var i=9; i < 18; i++){
+      if(moment().format("H") == i){
+        $("#hour-"+ i).addClass("present");
+      }else if(moment().format("H") > i){
+        $("#hour-" + i).addClass("past");
+      }else{
+        $("#hour-" + i).addClass("future");
+      };
+    }
+  }, 1000);
+
+ 
+ 
+  //Click event target for submit button
+$(".saveBtn").on("click", function(){
+  var key = $(this).parent().attr("data-hour");
+  var value = $(this).siblings(".description").val();
+  localStorage.setItem(key, value);
+})
 
 
